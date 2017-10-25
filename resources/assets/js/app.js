b63ccdd81163e8f2d56df35b2496f0de7e27d6ce
Alex <climber.business@gmail.com>
2017-10-25 23:19:00 +0300

    first vue + onsen app test

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,22 +1,31 @@
+import Vue from 'vue';
+import VueOnsen from 'vue-onsenui';
 
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
+Vue.use(VueOnsen);
 
-require('./bootstrap');
+window.Vue = Vue;
 
-window.Vue = require('vue');
+const page2 = {
+    key: 'page2',
+    template: '#page2'
+};
 
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
+const page1 = {
+    key: 'page1',
+    template: '#page1',
+    methods: {
+        push() {
+            this.$emit('push-page', page2);
+        }
+    }
+};
 
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
-
-const app = new Vue({
-    el: '#app'
+new Vue({
+    el: '#app',
+    template: '#main',
+    data() {
+        return {
+            pageStack: [page1]
+        };
+    }
 });
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,92 +4,43 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        <link rel="stylesheet" href="css/app.css">
         <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
-                </div>
-            @endif
+        <template id="main">
+            <v-ons-navigator swipeable
+                             :page-stack="pageStack"
+                             v-on:push-page="pageStack.push($event)"
+            ></v-ons-navigator>
+        </template>
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
+        <template id="page1">
+            <v-ons-page>
+                <v-ons-toolbar>
+                    <div class="center">Страничка намбер уан</div>
+                </v-ons-toolbar>
+                <p style="text-align: center">
+                    Тут можено перейти
+                    <v-ons-button v-on:click="push">На другую страничку</v-ons-button>
+                </p>
+            </v-ons-page>
+        </template>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
-        </div>
+        <template id="page2">
+            <v-ons-page>
+                <v-ons-toolbar>
+                    <div class="left">
+                        <v-ons-back-button>Страничка намбер уан</v-ons-back-button>
+                    </div>
+                    <div class="center">Другая страничка</div>
+                </v-ons-toolbar>
+                <p style="text-align: center">А это страничка специально для Людочки! ;-*</p>
+            </v-ons-page>
+        </template>
+
+        <div id="app"></div>
+
+        <script src="js/app.js"></script>
     </body>
 </html>
