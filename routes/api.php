7c7b25fb50227775f43708bd2c79aa38cd5eee0d
Alexander Akimov <climber.business@gmail.com>
2017-11-10 14:24:29 +0300

    refactoring routes

--- routes/api.php
+++ routes/api.php
@@ -1,16 +1,14 @@
 <?php
 
-Route::group(['middleware' => 'web'], function () {
-    Route::group(['middleware' => 'auth'], function () {
-        Route::get('/climbs', 'ClimbSessionController@index');
-        Route::get('/climbs/{climbSession}', 'ClimbSessionController@show')
-            ->where('climbSession', '[0-9]+');
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/climbs', 'ClimbSessionController@index');
+    Route::get('/climbs/{climbSession}', 'ClimbSessionController@show')
+        ->where('climbSession', '[0-9]+');
 
-        Route::post('/climbs', 'ClimbSessionController@store');
+    Route::post('/climbs', 'ClimbSessionController@store');
 
-        Route::post('/climbed-routes', 'ClimbedRouteController@store');
-    });
-
-    Route::get('/settings', 'SettingsController@index');
-    Route::post('/session', 'SessionController@store');
+    Route::post('/climbed-routes', 'ClimbedRouteController@store');
 });
+
+Route::get('/settings', 'SettingsController@index');
+Route::post('/session', 'SessionController@store');
